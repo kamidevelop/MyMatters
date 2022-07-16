6b83abfb0243cf6c482660a0bee8bbe18dbb22b0
Kamikotonaru <kezin.danil@yandex.ru>
2022-07-16 16:46:50 +0700

    Dev func writeInFamilyData completed

--- app/src/main/java/com/example/mymatters/mymattersdatabase/MyMattersDatabase.kt
+++ app/src/main/java/com/example/mymatters/mymattersdatabase/MyMattersDatabase.kt
@@ -7,8 +7,19 @@ data class Person(val personName : String, val photoUri : String, val wallet : D
 
 class MyMattersDatabase{
     companion object {
+        const val DATA_BASE_NAME = "mymattersfinance.db"
         fun writeInFamilyData(person: Person, context: Context) {
-
+            val database : SQLiteDatabase = context.openOrCreateDatabase(DATA_BASE_NAME,Context.MODE_PRIVATE,null)
+            if(database.isOpen){
+                val contentValues = ContentValues()
+                contentValues.put("name", person.personName)
+                contentValues.put("photoUri",person.photoUri)
+                contentValues.put("wallet",person.wallet)
+                contentValues.put("coinbox",person.coinbox)
+                contentValues.put("wallet",person.wallet)
+                database.insert("Family",null,contentValues)
+                database.close()
+            }
         }
     }
 }
