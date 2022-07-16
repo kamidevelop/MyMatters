6fb2531c8601b7da1f3f5ba678583e29650620c8
Kamikotonaru <kezin.danil@yandex.ru>
2022-07-16 16:40:14 +0700

    Rename, add imports
    Rename func, make it as companion object, necessary imports

--- app/src/main/java/com/example/mymatters/mymattersdatabase/MyMattersDatabase.kt
+++ app/src/main/java/com/example/mymatters/mymattersdatabase/MyMattersDatabase.kt
@@ -1,9 +1,14 @@
 package com.example.mymatters.mymattersdatabase
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 
 data class Person(val personName : String, val photoUri : String, val wallet : Double, val coinbox : Double)
 
 class MyMattersDatabase{
-    fun AddInFamily(person: Person){
-        
+    companion object {
+        fun writeInFamilyData(person: Person, context: Context) {
+
+        }
     }
 }
