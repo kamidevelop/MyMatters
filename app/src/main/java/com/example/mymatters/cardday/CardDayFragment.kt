e9fe30aed31401add8d3d871614e6fb75fa316f9
Kamikotonaru <kezin.danil@yandex.ru>
2022-08-25 14:16:35 +0700

    Complete dev basic interface
    Added:
    ViewPager2 and basic scheme item of
    Icons of wallet, coinbox
    String resource lables

--- app/src/main/java/com/example/mymatters/cardday/CardDayFragment.kt
+++ app/src/main/java/com/example/mymatters/cardday/CardDayFragment.kt
@@ -6,7 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.example.mymatters.R
+class CardDayPagerVH(itemView : View) : RecyclerView.ViewHolder(itemView)
+
+class CardDayViewPagerAdapter : RecyclerView.Adapter<CardDayPagerVH>(){
+    val texts = arrayListOf<CharSequence>("Тут будет рейтинг","Тут будут расходы","Тут будут доходы")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardDayPagerVH =
+        CardDayPagerVH(LayoutInflater.from(parent.context).inflate(R.layout.cardday_info_item,parent,false))
+
+    override fun onBindViewHolder(holder: CardDayPagerVH, position: Int) = holder.itemView.run{
+        this.findViewById<TextView>(R.id.info_text).text = texts[position]
+    }
+
+    override fun getItemCount(): Int {
+        return texts.size
+    }
+
+}
 
 class CardDayFragment : Fragment() {
 
@@ -20,7 +39,10 @@ class CardDayFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_card_day, container, false)
+        val inf = inflater.inflate(R.layout.fragment_card_day, container, false)
+        val vp = inf.findViewById<ViewPager2>(R.id.cardday_info_viewpager)
+        vp.adapter = CardDayViewPagerAdapter()
+        return inf
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
