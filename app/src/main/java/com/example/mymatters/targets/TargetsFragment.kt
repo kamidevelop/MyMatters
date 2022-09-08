447e47c7b274bee9cc232fd85a31af98df58f7ce
Kamikotonaru <kezin.danil@yandex.ru>
2022-09-09 02:32:32 +0700

    Invoke new dialog

--- app/src/main/java/com/example/mymatters/targets/TargetsFragment.kt
+++ app/src/main/java/com/example/mymatters/targets/TargetsFragment.kt
@@ -1,12 +1,35 @@
 package com.example.mymatters.targets
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mymatters.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class TargetRecyclerVH(itemView : View) : RecyclerView.ViewHolder(itemView)
+
+/*class TargetRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    var targets = ""
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+
+}*/
 
 class TargetsFragment : Fragment() {
 
@@ -15,12 +38,23 @@ class TargetsFragment : Fragment() {
     }
 
     private lateinit var viewModel: TargetsViewModel
-
+    private lateinit var dialog : Dialog
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_targets, container, false)
+        val root = inflater.inflate(R.layout.fragment_targets, container, false)
+        val fab = root.findViewById<FloatingActionButton>(R.id.fab_add_new_target)
+        fab.setOnClickListener({
+            val builder = AlertDialog.Builder(root.context)
+            dialog = builder.setTitle(resources.getString(R.string.title_create_target))
+                .setView(R.layout.target_add_record)
+                .setPositiveButton(resources.getString(R.string.button_save),null)
+                .setNegativeButton(resources.getString(R.string.button_cancel),null)
+                .create()
+            dialog.show()
+        })
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
